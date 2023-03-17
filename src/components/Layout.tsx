9e6e5e85ba31c92724a7fe287547fddc4833f62e
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-03-17 16:31:58 -0500

    Add: Layout Component and Fix: next.config.mjs

---
+++ src/components/Layout.tsx
@@ -0,0 +1,27 @@
+import Head from "next/head";
+
+type Props = {
+  title?: string;
+  description?: string;
+  children: React.ReactNode;
+};
+
+function Layout({
+  children,
+  title = "Confesiones UPC",
+  description = "Una página web donde podrás confesarnos lo que desees sobre lo que pasa dentro de la universidad.",
+}: Props) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="min-h-screen bg-red-200">{children}</main>
+    </>
+  );
+}
+
+export default Layout;
