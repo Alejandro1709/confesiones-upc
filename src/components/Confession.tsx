6e5552d700d056bd184a3c5d5c1038d8978a8d20
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-03-18 17:59:06 -0500

    Add: utils for slugifying and deslugifying

--- src/components/Confession.tsx
+++ src/components/Confession.tsx
@@ -3,6 +3,7 @@
 import { useId } from "react";
 import Link from "next/link";
 import type IConfession from "@/types/confession";
+import { slugify } from "@/utils/confessions";
 
 type Props = {
   confession: IConfession;
@@ -10,19 +11,9 @@ type Props = {
 
 function Confession({ confession }: Props) {
   const id = useId();
-  const slugify = (str: string) => {
-    if (!str) return `${confession.id}${id}}`;
-    return str
-      .toString()
-      .normalize("NFD")
-      .replace(/[\u0300-\u036f]/g, "")
-      .toLowerCase()
-      .trim()
-      .replace(/[\s\W-]+/g, "-");
-  };
 
   return (
-    <Link href={`/confesiones/${slugify(confession.title)}`}>
+    <Link href={`/confesiones/${slugify(confession.title, confession, id)}`}>
       <article className="flex h-[187px] w-[383px] cursor-pointer select-none flex-col items-center justify-center border bg-white p-2 transition-all hover:scale-95">
         <p className="text-center text-lg font-medium">{confession.content}</p>
       </article>
