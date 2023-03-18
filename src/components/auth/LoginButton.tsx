f266139e7090d8700135c78d7001064e05ad67c5
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-03-18 13:26:14 -0500

    Add: env examples and Add: auth secret

--- src/components/auth/LoginButton.tsx
+++ src/components/auth/LoginButton.tsx
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 "use client";
+
+import { signIn } from "next-auth/react";
 
 function LoginButton() {
   return (
-    <button className="rounded-full bg-gray-700 p-2 text-xs text-white hover:bg-gray-800">
+    <button
+      className="rounded-full bg-gray-700 p-2 text-xs text-white hover:bg-gray-800"
+      onClick={() => signIn()}
+    >
       Iniciar Sesión
     </button>
   );
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,12 +3,15 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 
-export default NextAuth({
+export const authOptions = {
   adapter: PrismaAdapter(prisma),
+  secret: process.env.AUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-});
+};
+
+export default NextAuth(authOptions);
