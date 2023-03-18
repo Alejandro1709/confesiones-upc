7f79f3a26bfed129df9ad763d7f6f8c858bdcb98
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2023-03-18 13:02:00 -0500

    Add: Google Secrets

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "@/lib/prisma";
 
 export default NextAuth({
   adapter: PrismaAdapter(prisma),
